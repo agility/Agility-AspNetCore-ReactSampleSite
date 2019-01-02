fff7e5cbecc9f5de484ddfbb4a667cc7838bc5bf
James Vidler <james.vidler@outlook.com>
2019-01-02 16:20:00 -0500

    added ugc via npm

--- Website/Startup.cs
+++ Website/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
-
 using Agility.Web;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
--- Website/wwwroot/src/js/index.js
+++ Website/wwwroot/src/js/index.js
@@ -1,7 +1,6 @@
 import '../scss/global.scss'
-import agility from 'agility'
+import ugcClient from 'agility-ugc'
 
-document.getElementById('test').onclick = function() {
-    alert('hello ' + agility.UniqueID('fromAgilityNPM-'));
-}
-
+ugcClient.GetSettings(function(resp) {
+    console.log(resp);
+})
