78fa7d6373fb6794cca44cc1a2ef9b0c4e3116fa
Joel Varty <joelvarty@outlook.com>
2019-01-25 09:32:31 -0500

    updated agilitywebcore and contact us

---
+++ Website/ViewComponents/Modules/ContactUsViewComponent.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Website.AgilityModels;
+using Website.Extensions;
+using Agility.Web.Extensions;
+
+namespace Website.ViewComponents.Modules
+{
+	public class ContactUs : ViewComponent
+	{
+
+		public Task<IViewComponentResult> InvokeAsync(Module_ContentPanel module)
+		{
+			return Task.Run<IViewComponentResult>(() =>
+			{
+
+				var panel = module.Panel.GetByID(module.PanelID).ToFrontendProps();
+
+				return new ReactViewComponentResult("Components.ContentPanel", panel);
+			});
+		}
+
+	}
+
+}
